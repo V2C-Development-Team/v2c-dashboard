c3c26701ced44b4a2a482e7a4a9197215da1705a
comphonia <online.comphonia@gmail.com>
2020-09-25 01:27:39 -0500

    persist theme prefs using localstorage

--- src/Start.js
+++ src/Start.js
@@ -1,20 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Start.scss';
 import { ThemeProvider } from '@material-ui/core/styles';
 import getTheme from './utils/theme';
 import Layout from './components/Layout/Layout';
 import { StylesProvider } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { ThemeContext } from './context/themeContext';
 
 const Start = () => {
     const [themeColor, setThemeColor] = useState('light');
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
     const toggleThemeColor = () => {
         setThemeColor((themeColor) =>
+            themeColor === 'light' ? 'dark' : 'light'
+        );
+        localStorage.setItem(
+            'theme',
             themeColor === 'light' ? 'dark' : 'light'
         );
     };
 
     React.createContext({ themeColor, toggleThemeColor });
+
+    useEffect(() => {
+        let tempThemeColor = prefersDarkMode ? 'dark' : 'light';
+        const themePref = localStorage.getItem('theme');
+        if (themePref && themePref === ('dark' || 'light')) {
+            tempThemeColor = themePref;
+        }
+        setThemeColor(tempThemeColor);
+    }, [prefersDarkMode]);
 
     return (
         <ThemeProvider theme={getTheme(themeColor)}>

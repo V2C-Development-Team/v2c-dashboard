3a99c21c2fe55b8be07d687d4a6fcf6153282393
comphonia <online.comphonia@gmail.com>
2020-09-25 19:53:26 -0500

    integrated gaming-poc using an iframe relay

--- src/app/pages/Home/Home.js
+++ src/app/pages/Home/Home.js
@@ -2,18 +2,29 @@ import React, { Fragment } from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import PinnedCard from '../../components/Cards/PinnedCard';
 import ToolBar from '../../components/ToolBar/ToolBar';
+import FrameDialog from '../../components/FrameDialog/FrameDialog';
 
 const Home = (props) => {
+    const [isFrameOpen, setIsFrameOpen] = React.useState(false);
+
     return (
         <Fragment>
             <ToolBar crumbs={['Home']} />
+            <FrameDialog
+                isFrameOpen={isFrameOpen}
+                setIsFrameOpen={setIsFrameOpen}
+            />
             <Typography variant="h5">Pinned Connections</Typography>
             <Grid container spacing={3} style={{ marginTop: 15 }}>
                 <Grid item xs={12} md={3}>
-                    <PinnedCard type="Game" title="Simon" />
+                    <PinnedCard
+                        type="Game"
+                        title="Gaming Station"
+                        onConnect={() => setIsFrameOpen(true)}
+                    />
                 </Grid>
                 <Grid item xs={12} md={3}>
-                    <PinnedCard type="Game" title="8Puzzle" />
+                    <PinnedCard empty />
                 </Grid>
                 <Grid item xs={12} md={3}>
                     <PinnedCard empty />

46ce846f6c3283140efd0c3255e28c2b2058c793
comphonia <online.comphonia@gmail.com>
2020-10-09 07:32:41 -0500

    updated api to accomodate more services

--- src/services/api.js
+++ src/services/api.js
@@ -1,53 +1,62 @@
-import axios from "axios";
-import auth from "../whoami/auth";
+import axios from 'axios';
+import auth from '../whoami/auth';
 
 export const apiErrorMsg =
-  "The server cannot be reached at this time, please try again later.";
+    'The server cannot be reached at this time, please try again later.';
 
 export const CancelToken = axios.CancelToken;
 // eslint-disable-next-line
 const getAuthHeader = () => {
-  const email = auth.getEmail();
-  const password = atob(auth.getPassword());
-  const authHeader = btoa(`${email}:${password}`);
-  return authHeader;
+    const email = auth.getEmail();
+    const password = atob(auth.getPassword());
+    const authHeader = btoa(`${email}:${password}`);
+    return authHeader;
 };
 
+export const dispatcherService = axios.create({
+    baseURL: process.env.REACT_APP_DISPATCHER_URL,
+    headers: { 'Cache-Control': 'no-cache' },
+    responseType: 'json',
+    timeout: 60000,
+});
+
 const service = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
-  headers: { "Cache-Control": "no-cache" },
-  responseType: "json",
-  timeout: 60000,
-  isDelayed: false,
-  isAuthorization: false,
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    headers: { 'Cache-Control': 'no-cache' },
+    responseType: 'json',
+    timeout: 60000,
+    isDelayed: false,
+    isAuthorization: false,
 });
 
 service.interceptors.request.use((config) => {
-  if (config.isDelayed) {
-    return new Promise((resolve) => setTimeout(() => resolve(config), 3000));
-  }
-  if (config.isAuthorization) {
-    config.headers.Authorization = "Basic " + getAuthHeader();
-  } else {
-    config.headers["X-Access-Token"] = auth.getSessionToken();
-  }
+    if (config.isDelayed) {
+        return new Promise((resolve) =>
+            setTimeout(() => resolve(config), 3000)
+        );
+    }
+    if (config.isAuthorization) {
+        config.headers.Authorization = 'Basic ' + getAuthHeader();
+    } else {
+        config.headers['X-Access-Token'] = auth.getSessionToken();
+    }
 
-  /*     if (config.hasCAPTCHA) {
+    /*     if (config.hasCAPTCHA) {
         const captcha = auth.getReCAPTCHA();
         config.headers['X-G-RECAPTCHA-RESPONSE'] = captcha;
     } */
 
-  return config;
+    return config;
 });
 
 service.interceptors.response.use((config) => {
-  if (config.headers["x-access-token"]) {
-    auth.createSession(config.headers["x-access-token"]);
-  }
-  if (config.status === 401) {
-    auth.logout();
-  }
-  return config;
+    if (config.headers['x-access-token']) {
+        auth.createSession(config.headers['x-access-token']);
+    }
+    if (config.status === 401) {
+        auth.logout();
+    }
+    return config;
 });
 
 export default service;
